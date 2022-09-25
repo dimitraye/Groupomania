c3151d3a640ffcbe01d878983da5e4fc07edcad4
dimitraye <dimitraye94@gmail.com>
2022-09-25 23:06:31 +0200

    Ajout des commentaires dans le dossier routes

--- back/routes/user.js
+++ back/routes/user.js
@@ -1,9 +1,11 @@
 /* ROUTES */
 /* Relie les requêtes à la méthode du controlleur qui leur correspondent */
 
+//Importe le package express qui est un framework facilitant la gestion de serveur node.js
 const express = require('express');
+//Création d'un noueau router
 const router = express.Router();
-
+//Importe le fichier user depuis le dossier controllers 
 const userCtrl = require('../controllers/user');
 
 //relie la route signup à la méthode signup du controller(creation d'un utilisateur)
@@ -11,4 +13,5 @@ router.post('/signup', userCtrl.signup);
 //relie la route login à la méthode login du controller(connexion d'un utilisateur)
 router.post('/login', userCtrl.login);
 
+//Rend le module accessible dans d'autres fichiers
 module.exports = router;
