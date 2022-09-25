25059297af1bd1e7faa42ebdcfcfaa6d01e63e93
dimitraye <dimitraye94@gmail.com>
2022-09-25 21:27:38 +0200

    ajout des commentaires dans les fichiers du contolleur

--- back/controllers/user.js
+++ back/controllers/user.js
@@ -1,5 +1,8 @@
+//Importe le modèle user pour créer un utilisateur
 const User = require("../models/user");
+//Importe le module bcrypt servant à crypter/décrypter un mot de passe
 const bcrypt = require("bcrypt"); //gère le hachage du mdp
+//Importe le module jsonwebtoken servant à attribuer un tokken d'authentification à un utilisateur
 const jwt = require("jsonwebtoken"); //Génère le token d'authentification
 //Module servant à vérifier la validité d'un email
 const emailvalidator = require("email-validator");
@@ -16,14 +19,19 @@ schema
 exports.signup = (req, res, next) => {
   //Vérifie si le format de l'email est valide
   if (!req.body.email || !emailvalidator.validate(req.body.email)) {
+    //Si l'email n'est pas valide on renvoit un message d'erreur
     res.status(400).send({ error: "Email ou Mot de Passe Invalide" });
+    //Génère une exception étant une chaîne de caractères
     throw "Erreur : Email ou Mot de Passe Invalide";
   }
   //Vérifie si le format du mot de passe est valide
   if ((!req.body.password || !schema.validate(req.body.password))){
+    //Si le mdp n'est pas valide on renvoit un message d'erreur
     res.status(400).send({error:"Merci d'indiquer un mot de passe de minimum 5 caractères"});
+    //Génère une exception étant une chaîne de caractères
     throw "Erreur : Mot de passe de moins de 5 caractères";
   }
+  //hachage du mdp
   bcrypt
     .hash(req.body.password, 10)
     .then((hash) => {
@@ -34,7 +42,9 @@ exports.signup = (req, res, next) => {
       //sauvegarde l'utilisateur
       user
         .save()
-        .then(() => res.status(201).json({ message: "Utilisateur créé !" }))
+          //Renvoie une réponse dont le status est 200 et dont le message est au format JSON
+          .then(() => res.status(201).json({ message: "Utilisateur créé !" }))
+        //La méthode catch() renvoie un objet Promise et ne traite que des cas où la promesse initiale est rejetée
         .catch((error) =>
           res
             .status(400)
@@ -42,24 +52,34 @@ exports.signup = (req, res, next) => {
         );
     })
     .catch((error) =>
+      //Renvoie une réponse dont le status est 500 et dont le message est au format JSON
       res.status(500).json({ error: "Erreur lors du cryptage du mot de passe" })
     );
 };
 
 //Connexion
 exports.login = (req, res, next) => {
+  //La méthode findOne() est utilisée pour chercher si l'email se trouve dans le base de données
   User.findOne({ email: req.body.email })
     .then((user) => {
+      //Si l'email entré ne se trouve pas dans la BD
       if (!user) {
+        //Retoure une réponse dont le status est 401 et dont le message est au format JSON
         return res.status(401).json({ error: "Utilisateur non trouvé !" });
       }
       //compare  si le mdp de la requête correspond au mdp dans la BD
       bcrypt
         .compare(req.body.password, user.password)
         .then((valid) => {
+          //Cas 1
+          //Le mot de passe entré n'est pas valide
           if (!valid) {
+            //Retoure une réponse dont le status est 401 et dont le message est au format JSON
             return res.status(401).json({ error: "Mot de passe incorrect !" });
           }
+          //Cas 2
+          //Le mot de passe entré est valide
+          //Retoure une réponse dont le status est 200 avec la propriété userId et token au format JSON
           res.status(200).json({
             userId: user._id,
             role: user.role,
@@ -73,10 +93,12 @@ exports.login = (req, res, next) => {
           });
         })
         .catch((error) =>
+          //Retoure une réponse dont le status est 500 et dont le message est au format JSON
           res.status(500).json({ error: "Email ou mot de passe érroné" })
         );
     })
     .catch((error) =>
+      //Retoure une réponse dont le status est 500 et dont le message est au format JSON
       res.status(500).json({ error: "Email ou mot de passe érroné" })
     );
 };
