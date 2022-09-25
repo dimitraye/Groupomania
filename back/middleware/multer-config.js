d04eecd4a24f9f1ac8967f624d551200b2d92cdc
dimitraye <dimitraye94@gmail.com>
2022-09-25 22:36:47 +0200

    ajout des commentaires dans le dossier middleware

--- back/middleware/multer-config.js
+++ back/middleware/multer-config.js
@@ -1,15 +1,23 @@
+//Gère la gestion des fichiers 
+
+//Importe le module multer pour la gestion desfichiers
 const multer = require('multer');
 
+//Objet ayant comme propriété les formats d'image que l'on a choisi de traiter 
+//Les valeurs de cet objet sont les extensions du fichier (jpg, jpeg, png)
 const MIME_TYPES = {
     'image/jpg': 'jpg',
     'image/jpeg': 'jpg',
     'image/png': 'png'
 };
 
+//diskStorage permet de gérer le stockage de fichier ( fichier image dans notre cas)
 const storage = multer.diskStorage({
+    //Dossier de destination (images)
     destination: (req, file, callback) => {
         callback(null, 'images');
     },
+    //Gestion du renomage du fichier
     filename: (req, file, callback) => {
         const name = file.originalname.split(' ').join('_');
         const extension = MIME_TYPES[file.mimetype];
@@ -17,4 +25,5 @@ const storage = multer.diskStorage({
     }
 });
 
+//Rend le module multer accessible dans d'autres fichiers
 module.exports = multer({ storage: storage }).single('image');
